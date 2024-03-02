42025f65052a729872782d088589a5df6db93525
NivedHari <nivedhari44@gmail.com>
2024-03-02 12:14:00 +0530

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const express_2 = require("express");
+let todos = [];
+const router = (0, express_2.Router)();
+router.use(express_1.default.json());
+router.get("/", (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post("/todo", (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text,
+    };
+    todos.push(newTodo);
+    res.status(201).json({ message: "Added Todo", todo: newTodo, todos: todos });
+});
+router.put("/todo/:todoId", (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        return res.status(200).json({ message: "updated Todo", todos: todos });
+    }
+    return res.status(404).json({ message: "Could Not Find the Item" });
+});
+router.delete("/todo/:todoId", (req, res, next) => {
+    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    res.status(200).json({ message: "Deleted Todo", todos: todos });
+});
+exports.default = router;
